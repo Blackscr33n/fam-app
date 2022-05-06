7cebf555de78fa811e7d93366f9fd0295a7cfba7
Bernhard Miehl <bernhard_tulln@live.at>
2022-05-06 10:00:38 +0200

    add store setup

--- src/app/store/actions.ts
+++ src/app/store/actions.ts
@@ -1,11 +1,16 @@
 import { createAction, props } from '@ngrx/store';
 
 export const login = createAction(
-  '[Login Page] Login',
-  props<{ username: string; password: string }>()
+    '[Login Page] Login',
+    props<{ username: string; password: string }>()
+);
+
+export const loginSuccess = createAction(
+    '[Login Page] Login Success'
 );
 
 export const register = createAction(
-  '[Register Page] Register',
-  props<{ username: string; password: string }>()
+    '[Register Page] Register',
+    props<{ username: string; password: string }>()
 );
+
--- src/app/store/reducer.ts
+++ src/app/store/reducer.ts
@@ -1,18 +1,18 @@
-import { createFeatureSelector, createReducer } from '@ngrx/store';
+import { createReducer } from '@ngrx/store';
 import { Family, Purchase, User } from 'src/app/_models';
 
 export interface State {
-  family: Family;
-  purchases: Purchase[];
-  user: User;
-  isLoggedIn: boolean;
+    family: Family;
+    purchases: Purchase[];
+    user: User;
+    isLoggedIn: boolean;
 }
 
 export const initialState: State = {
-  family: null,
-  purchases: [],
-  user: null,
-  isLoggedIn: false,
+    family: null,
+    purchases: [],
+    user: null,
+    isLoggedIn: false,
 };
 
 export const familyFinanceReducer = createReducer(initialState);
